8706dd33efc74f555eba3d03c2e764c211d4d528
leeauk21 <leeauk21@gmail.com>
2022-12-12 22:19:54 +0000

    downdate

--- AdaptiveFilter.py
+++ AdaptiveFilter.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class EWRLS:
 
-    def __init__(self,lamb,delta,dim,rolling_window = False):
+    def __init__(self,lamb,delta,dim,rolling_window = 180):
         """
         Description:
         lamb : forgetting factor
@@ -16,7 +16,10 @@ class EWRLS:
         self.P = 1 / delta * np.identity(self.dim)
         self.window = rolling_window
         self.n = 0
+        # storing all past observations
         self.U = 0
+        # storing all past response
+        self.D = 0
 
     def update(self,u,d):
         """
@@ -32,21 +35,27 @@ class EWRLS:
         # update w
         self.w = self.w + k @ xi.T
         self.P = self.lamb ** (-1) * self.P - self.lamb ** (-1) * k @ u.T @ self.P
-        # if self.n == 0:
-        #     self.U = xi
-        # if self.n > 0:
-        #     self.U = np.hstack((self.U,xi))
+        if self.n == 0:
+            self.U = u
+            self.D = d
+        if self.n > 0:
+            self.U = np.hstack((self.U,u))
+            self.D = np.hstack((self.D,d))
         self.n += 1
 
-    # # TODO write downdate
-    # def downdate(self):
-    #     u0 = self.U[:,0]
-    #     k = self.P @ u0 / (self.lamb ** (self.window + 1) + u0.T @ self.P @ self.u0)
-    #     self.P = self.P - k @ u0.T @ self.P
-    #     self.w 
+    def downdate(self):
+        u0 = np.reshape(self.U[:,0],(5,-1))
+        d0 = np.reshape(self.D[:,0],(1,-1))
+        k_downdate = self.lamb ** (self.window + 1) * self.P @ u0 / (1 - self.lamb ** (self.window + 1) * u0.T @ self.P @ u0)
+        self.w = self.w + k_downdate @ (u0.T @ self.w - d0)
+        self.P = self.P + k_downdate @ u0.T @ self.P
+        self.n -= 1
 
-    # def step(self,u,d):
-    #     self.update()
+    def step(self,u,d):
+        self.update(u,d)
+        if self.n > self.window:
+            self.downdate()
+
 
     def predict(self,u):
         return (self.w.T @ u).item()
